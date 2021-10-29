867a471cd611aba22702d353eb2189d893695d94
alcelia <adityo.p27@gmail.com>
2021-10-29 15:18:16 +0700

    modified:   ../.vs/dns-query-io/v15/.suo 	new file:   ../content_db_smartcare.xlsx 	modified:   dns_query_io.py 	new file:   ../dns_record_compiled_fuzzyCDN.xlsm 	new file:   ../dns_record_compiled_fuzzyCDN_rev1.xlsm 	new file:   ../fuzzmatch.xlsx 	new file:   ../fuzzmatch_result.xlsx 	new file:   ../fuzzmatch_result_rev1.xlsx 	new file:   ../nslookup_result.xlsx 	new file:   ../nslookup_result_rev1.xlsx

--- dns-query-io/dns-query-io/dns_query_io.py
+++ dns-query-io/dns-query-io/dns_query_io.py
@@ -1,9 +1,14 @@
 import dns.resolver
 import pandas as pd
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 inputdata = list()
 outputdata = list()
-filepath = 'C://Users/adity/source/repos/dns-query-io/dns_record_compiled.xlsm'
+input_filepath = "../dns_record_compiled.xlsm"
+contentdb_filepath = "../content_db_smartcare.xlsx"
+output_match_filepath = "../fuzzmatch_result.xlsx"
+output_nslookup_filepath = "../nslookup_result.xlsx"
 
 def resolver(url,i):
 	result = list()
@@ -26,20 +31,43 @@ def resolver(url,i):
 		print ('resolved record ',i)
 	return result
 	
-
-def xls_read(fp):
+# Function to read excel file source
+def xls_read(fp,colname):
 	df = pd.read_excel(fp,sheet_name=0)
-	ql = df['Domain'].tolist()
+	ql = df[colname].tolist()
 	return ql
 
-def xls_write(rl):
+# Function to write excel file (list only)
+def xls_write(rl,fp):
 	df = pd.DataFrame(rl)
-	df.to_excel('C://Users/adity/source/repos/dns-query-io/cname-result.xlsx', index=False)
+	df.to_excel(fp, index=False)
 
 idx = 0
-inputdata = xls_read(filepath)
+inputdata = xls_read(input_filepath,'Domain')
+db_content_data = xls_read(contentdb_filepath,'APP_NAME')
+
 for domain in inputdata:
 	idx += 1
 	outputdata.append(resolver(domain,idx))
 
-xls_write(outputdata)
+xls_write(outputdata,output_nslookup_filepath)
+
+idx = 0
+fuzzresults_ratio = list()
+fuzzresults_content = list()
+for target_url in inputdata:
+	matchrate=0
+	content_owner=""
+	for db_url in db_content_data:
+		if matchrate < fuzz.token_set_ratio(target_url,db_url):
+			matchrate = fuzz.token_set_ratio(target_url,db_url)
+			content_owner = db_url
+	print ('fuzzy checked record ',idx)
+	idx += 1
+	fuzzresults_ratio.append(matchrate)
+	fuzzresults_content.append(content_owner)
+
+
+matchdata = zip(fuzzresults_content,fuzzresults_ratio)
+xls_write(matchdata,output_match_filepath)
+
